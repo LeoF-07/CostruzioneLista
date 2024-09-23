84bd78fdca699f041ba7dfb8e86f3f5b732a54ca
Fortin Leonardo <leonardo.fortin@cz.lan>
2024-09-23 08:59:05 +0200

    Corretta2

---
+++ src/costruzione_lista/Opzione.java
@@ -0,0 +1,23 @@
+package costruzione_lista;
+
+public enum Opzione {
+
+    EXIT(0, "Termina programma"),
+
+    INSERIMENTO_IN_TESTA(1, "Inserisci elemento in testa"),
+    INSERIMENTO_IN_CODA(2, "Inserisci elemento in coda"),
+    INSERIMENTO_IN_UNA_POSIZIONE(3, "Inserisci elemento in una posizione"),
+
+    RIMOZIONE_IN_TESTA(4, "Rimuovi elemento in testa"),
+    RIMOZIONE_IN_CODA(5, "Rimuovi elemento in coda"),
+    RIMOZIONE_IN_UNA_POSIZIONE(6, "Rimuovi elemento in una posizione");
+
+    int numeroOpzione;
+    String descrizione;
+
+    Opzione(int numeroOpzione, String descrizione){
+        this.numeroOpzione = numeroOpzione;
+        this.descrizione = descrizione;
+    }
+
+}
--- src/costruzione_lista/TesterCostruzioneLista.java
+++ src/costruzione_lista/TesterCostruzioneLista.java
@@ -1,5 +1,7 @@
 package costruzione_lista;
 
+import java.util.Scanner;
+
 public class TesterCostruzioneLista {
 
     public static void main(String[] args) {
@@ -18,17 +20,72 @@ public class TesterCostruzioneLista {
         lista.inserimentoInTesta(antonio);
         lista.add(mattia, 4);
 
-        /*lista.inserimentoInTesta(mattia);
+        lista.inserimentoInTesta(mattia);
 
         lista.add(antonio, 3);
 
         lista.rimozioneInTesta();
         lista.rimozioneInCoda();
-        lista.remove(2);*/
+        lista.remove(2);
         
-        //lista.remove(5);
+        lista.remove(5);
 
         System.out.println(lista.toString());
+
+
+
+        /*descrizione();
+
+        int scelta;
+        do{
+            stampaMenu();
+            scelta = chiediIntero("Fai una scelta: ");
+            esegui(scelta);
+        }while(scelta != 0);
+
+        System.out.println("Programma terminato");*/
+    }
+
+    public static void descrizione(){
+        System.out.println("Implementazione lista");
+    }
+
+    public static void esegui(int scelta){
+        Opzione opzione;
+        try{
+            opzione = Opzione.values()[scelta];
+        }catch(ArrayIndexOutOfBoundsException ex){
+            return;
+        }
+
+        switch (opzione){
+            case EXIT:
+                break;
+            case INSERIMENTO_IN_TESTA:
+                break;
+            case INSERIMENTO_IN_CODA:
+                break;
+            case INSERIMENTO_IN_UNA_POSIZIONE:
+                break;
+            case RIMOZIONE_IN_TESTA:
+                break;
+            case RIMOZIONE_IN_CODA:
+                break;
+            case RIMOZIONE_IN_UNA_POSIZIONE:
+                break;
+        }
+    }
+
+    public static void stampaMenu(){
+        for (Opzione opzione : Opzione.values()){
+            System.out.println(opzione.numeroOpzione + ". " + opzione.descrizione);
+        }
+    }
+
+    public static int chiediIntero(String messaggio){
+        Scanner kbd = new Scanner(System.in);
+        System.out.print(messaggio);
+        return kbd.nextInt();
     }
 
 }
